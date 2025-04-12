d461d98a0a1c6dbd442eb13a17b6e42457de5933
morteza-mortezai <mrtz.mrtzi.1989@gmail.com>
2025-04-12 15:36:26 +0330

    update

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -3,8 +3,7 @@ import { Stack } from "expo-router";
 export default function RootLayout() {
   return (
     <Stack>
-      <Stack.Screen name="index" options={{ headerTitle: "Home",headerLeft:()=><></> }} />
-      <Stack.Screen name="about" options={{ headerTitle: "About" }} />
+      <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
 
       <Stack.Screen name="+not-found" />
     </Stack>
