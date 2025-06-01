75f1c7fb69bc24a306d49cf4940dc903e56db110
morteza-mortezai <mrtz.mrtzi.1989@gmail.com-at-003669155861>
2025-06-01 23:14:29 +0330

    update

--- app/(tabs)/explore.tsx
+++ app/(tabs)/explore.tsx
@@ -1,9 +1,12 @@
 import { ThemedText } from "@/components/ThemedText";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function TabTwoScreen() {
   return (
-    <ThemedText style={{ padding: 15, textAlign: "center" }}>
-      برای شادی روح تمامی درگذشتگان و اموات، فاتحه‌ای قرائت کنیم.
-    </ThemedText>
+    <SafeAreaView>
+      <ThemedText style={{ padding: 45, textAlign: "center" }}>
+        برای شادی روح تمامی درگذشتگان و اموات، فاتحه‌ای قرائت کنیم.
+      </ThemedText>
+    </SafeAreaView>
   );
 }
